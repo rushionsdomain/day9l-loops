5be0bad7e90e71c6f989439d5359d9df864b5ca0
Rushion <rushionchegge@gmail.com>
2024-06-20 10:29:32 +0300

    Learning loops

---
+++ loops.js
@@ -0,0 +1,11 @@
+let marks = 23;
+
+if (marks <= 100 && marks >= 70) {
+  console.log("A");
+} else if (marks <= 69 && marks >= 40) {
+  console.log("B");
+} else if (marks <= 40 && marks >= 0) {
+  console.log("C");
+} else {
+  console.log("fail");
+}
